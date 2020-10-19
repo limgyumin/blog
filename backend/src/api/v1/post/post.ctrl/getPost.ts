22a65f0eb002c2bf67c47cefcb761d00e44a39d1
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-10-19 09:18:39 +0900

    [ADD] Get Post

--- backend/src/api/v1/post/post.ctrl/getPost.ts
+++ backend/src/api/v1/post/post.ctrl/getPost.ts
@@ -1,3 +1,52 @@
-export default () => {
-    
-}
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import Post from "../../../../entity/Post";
+import logger from "../../../../lib/logger";
+
+export default async (req: Request, res: Response) => {
+  const idx: number = Number(req.params.idx);
+
+  if (isNaN(idx)) {
+    logger.yellow("[GET] 검증 오류. idx is NaN");
+    res.status(401).json({
+      status: 401,
+      message: "검증 오류.",
+    });
+    return;
+  }
+
+  try {
+    const postRepo = getRepository(Post);
+    const post = await postRepo.findOne({
+      where: {
+        idx,
+        is_deleted: false,
+      },
+    });
+
+    if (!post) {
+      logger.yellow("[GET] 글 없음.");
+      res.status(404).json({
+        status: 404,
+        message: "글 없음.",
+      });
+      return;
+    }
+
+    logger.green("[GET] 글 조회 성공.");
+    res.status(200).json({
+      status: 200,
+      message: "글 조회 성공.",
+      data: {
+        post,
+      },
+    });
+  } catch (error) {
+    logger.red("[GET] 글 조회 서버 오류.");
+    res.status(500).json({
+      status: 500,
+      message: "서버 오류.",
+    });
+    return;
+  }
+};
