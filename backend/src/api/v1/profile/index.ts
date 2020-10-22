71b5ac187fcca38349d54d482fd8170e39ad2d19
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-10-22 12:33:16 +0900

    [ADD] Get Profiles

--- backend/src/api/v1/profile/index.ts
+++ backend/src/api/v1/profile/index.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 import getProfile from "./profile.ctrl/getProfile";
 import modifyProfile from "./profile.ctrl/modifyProfile";
 import authMiddleWare from "../../../lib/middleware/auth";
+import getProfiles from "./profile.ctrl/getProfiles";
 
 const router = Router();
 
+router.get("/all", getProfiles);
 router.get("/", authMiddleWare.guest, getProfile);
 router.put("/", authMiddleWare.guest, modifyProfile);
 
---
+++ backend/src/api/v1/profile/profile.ctrl/getProfiles.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import User from "../../../../entity/User";
+import logger from "../../../../lib/logger";
+
+export default async (req: Request, res: Response) => {
+  try {
+    const userRepo = getRepository(User);
+    const [users, user_count] = await userRepo.findAndCount({
+      select: ["idx", "id", "name", "bio", "created_at"],
+      where: {
+        is_admin: false,
+      },
+    });
+
+    logger.green("[GET] 프로필 목록 조회 성공.");
+    res.status(200).json({
+      status: 200,
+      message: "프로필 목록 조회 성공.",
+      data: {
+        user_count,
+        users,
+      },
+    });
+  } catch (error) {
+    logger.red("[GET] 프로필 목록 조회 서버 오류.");
+    res.status(500).json({
+      status: 500,
+      message: "서버 오류.",
+    });
+  }
+};
