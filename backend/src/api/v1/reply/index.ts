b5404775740ac7659d5ac0acc68b25aed7ed090e
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-10-24 22:27:15 +0900

    [ADD] Get Replies

--- backend/src/api/v1/reply/index.ts
+++ backend/src/api/v1/reply/index.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import authMiddleWare from "../../../lib/middleware/auth";
+import createReply from "./reply.ctrl/createReply";
+import getReplies from "./reply.ctrl/getReplies";
 
 const router = Router();
+
+router.post("/", authMiddleWare.user, createReply);
+router.get("/", authMiddleWare.guest, getReplies);
 
 export default router;
---
+++ backend/src/api/v1/reply/reply.ctrl/getReplies.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import Comment from "../../../../entity/Comment";
+import Post from "../../../../entity/Post";
+import Reply from "../../../../entity/Reply";
+import logger from "../../../../lib/logger";
+
+export default async (req: Request, res: Response) => {
+  const commentIdx = req.query.comment;
+
+  try {
+    const commentRepo = getRepository(Comment);
+    const comment = await commentRepo.findOne({
+      where: {
+        idx: commentIdx,
+      },
+    });
+
+    if (!comment) {
+      logger.yellow("[GET] 댓글 없음.");
+      res.status(404).json({
+        status: 404,
+        message: "댓글 없음.",
+      });
+      return;
+    }
+
+    const postRepo = getRepository(Post);
+    const post = await postRepo.findOne({
+      where: {
+        idx: comment.fk_post_idx,
+      },
+    });
+
+    if (!post) {
+      logger.yellow("[GET] 글 없음.");
+      res.status(404).json({
+        status: 404,
+        message: "글 없음.",
+      });
+      return;
+    }
+
+    const replyRepo = getRepository(Reply);
+    const replies: Reply[] = await replyRepo.find({
+      comment,
+    });
+
+    logger.green("[GET] 답글 목록 조회 성공.");
+    res.status(200).json({
+      status: 200,
+      message: "답글 목록 조회 성공.",
+      data: {
+        replies,
+      },
+    });
+  } catch (error) {
+    logger.red("[GET] 답글 목록 조회 서버 오류.", error.message);
+    res.status(500).json({
+      status: 500,
+      message: "서버 오류.",
+    });
+  }
+};
