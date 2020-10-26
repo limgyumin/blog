7ed36219d81eaa2fbc87e7295a59180f37636b7a
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-10-26 09:19:49 +0900

    [ADD] Delete Like

---
+++ backend/src/api/v1/like/like.ctrl/deleteLike.ts
@@ -0,0 +1,63 @@
+import { Response } from "express";
+import { getRepository } from "typeorm";
+import Like from "../../../../entity/Like";
+import Post from "../../../../entity/Post";
+import User from "../../../../entity/User";
+import logger from "../../../../lib/logger";
+import AuthRequest from "../../../../type/AuthRequest";
+
+export default async (req: AuthRequest, res: Response) => {
+  const idx: number = Number(req.params.idx);
+
+  if (isNaN(idx)) {
+    logger.yellow("[DELETE] 검증 오류. idx is NaN");
+    res.status(400).json({
+      status: 400,
+      message: "검증 오류.",
+    });
+    return;
+  }
+
+  const user: User = req.user;
+
+  try {
+    const likeRepo = getRepository(Like);
+    const like: Like = await likeRepo.findOne({
+      where: {
+        idx,
+      },
+    });
+
+    if (!like) {
+      logger.yellow("[DELETE] 좋아요 없음.");
+      res.status(404).json({
+        status: 404,
+        message: "좋아요 없음.",
+      });
+      return;
+    }
+
+    if (like.fk_user_idx !== user.idx && !user.is_admin) {
+      logger.yellow("[DELETE] 권한 없음.");
+      res.status(403).json({
+        status: 403,
+        message: "권한 없음.",
+      });
+      return;
+    }
+
+    await likeRepo.remove(like);
+
+    logger.green("[DELETE] 좋아요 삭제 성공.");
+    res.status(200).json({
+      status: 200,
+      message: "좋아요 삭제 성공.",
+    });
+  } catch (error) {
+    logger.red("[DELETE] 좋아요 삭제 서버 오류.", error.message);
+    res.status(500).json({
+      status: 500,
+      message: "서버 오류.",
+    });
+  }
+};
