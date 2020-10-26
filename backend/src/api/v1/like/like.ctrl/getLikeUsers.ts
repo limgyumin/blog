8aaa1f774b11bef921f87d63d2ffd10799f17d15
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-10-26 09:19:39 +0900

    [ADD] Get Like Users

---
+++ backend/src/api/v1/like/like.ctrl/getLikeUsers.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import Like from "../../../../entity/Like";
+import Post from "../../../../entity/Post";
+import User from "../../../../entity/User";
+import logger from "../../../../lib/logger";
+import LikeUserType from "../../../../type/LikeUserType";
+
+export default async (req: Request, res: Response) => {
+  const postIdx = req.query.post;
+
+  try {
+    const postRepo = getRepository(Post);
+    const post: Post = await postRepo.findOne({
+      where: {
+        is_temp: false,
+        idx: postIdx,
+      },
+    });
+
+    if (!post) {
+      logger.yellow("[GET] 글 없음.");
+      res.status(404).json({
+        status: 404,
+        message: "글 없음.",
+      });
+      return;
+    }
+
+    const likeRepo = getRepository(Like);
+    const [likes, like_count]: [
+      LikeUserType[],
+      number
+    ] = await likeRepo.findAndCount({
+      where: {
+        post,
+      },
+    });
+
+    for (let i in likes) {
+      const userRepo = getRepository(User);
+      const user: User = await userRepo.findOne({
+        where: {
+          idx: likes[i].fk_user_idx,
+        },
+      });
+
+      likes[i].user_name = user.name;
+    }
+
+    logger.green("[GET] 좋아요 목록 조회 성공.");
+    res.status(200).json({
+      status: 200,
+      message: "좋아요 목록 조회 성공.",
+      data: {
+        like_count,
+        likes,
+      },
+    });
+  } catch (error) {
+    logger.red("[GET] 좋아요 목록 조회 서버 오류.", error.message);
+    res.status(500).json({
+      status: 500,
+      message: "서버 오류.",
+    });
+  }
+};
---
+++ backend/src/type/LikeUserType.ts
@@ -0,0 +1,5 @@
+import Like from "../entity/Like";
+
+export default interface LikeUserType extends Like {
+  user_name?: string;
+}
