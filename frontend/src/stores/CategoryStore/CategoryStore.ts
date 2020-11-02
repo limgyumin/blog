b193ffd4f735b58bcc5b059dbc10aed8b78f5495
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-11-02 20:44:17 +0900

    [ADD] Category Store

---
+++ frontend/src/stores/CategoryStore/CategoryStore.ts
@@ -0,0 +1,42 @@
+import { action, observable } from "mobx";
+import { autobind } from "core-decorators";
+import Category from "../../assets/api/Category";
+
+interface CategoryResponseType {
+  status: number;
+  message: string;
+  data: {
+    total: number;
+    categories: CategoryType[];
+  };
+}
+
+interface CategoryType {
+  idx: number;
+  name: string;
+  order_number: number;
+  post_count: number;
+}
+
+@autobind
+class CategoryStore {
+  @observable categories: CategoryType[] = [];
+
+  @action
+  handleCategories = async (): Promise<CategoryResponseType> => {
+    try {
+      const response: CategoryResponseType = await Category.getCategories();
+      this.categories = response.data["categories"];
+
+      return new Promise((resolve, reject) => {
+        resolve(response);
+      });
+    } catch (error) {
+      return new Promise((resolve, reject) => {
+        reject(new Error(`${error}`));
+      });
+    }
+  };
+}
+
+export default CategoryStore;
--- frontend/src/stores/index.ts
+++ frontend/src/stores/index.ts
@@ -1,7 +1,9 @@
+import CategoryStore from "./CategoryStore";
 import PostStore from "./PostStore";
 
 const stores = {
   PostStore: new PostStore(),
+  CategoryStore: new CategoryStore(),
 };
 
 export default stores;
