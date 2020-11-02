4d1073ecd1e04eb8f95eacacf74afeee0680981a
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-11-02 20:09:59 +0900

    [ADD] Main Posts

---
+++ frontend/src/components/Main/MainPosts/MainPosts.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import MainPostItem from "./MainPostItem";
+import "./MainPosts.scss";
+
+interface MainPostsProps {
+  posts: PostType[];
+  loading: boolean;
+}
+
+interface PostType {
+  idx: number;
+  title: string;
+  description: string;
+  thumbnail: string;
+  created_at: Date;
+  comment_count: number;
+  like_count: number;
+}
+
+const MainPosts = ({ posts, loading }: MainPostsProps) => {
+  return (
+    <>
+      <div className="Main-Posts">
+        <div className="Main-Posts-Container">
+          {posts.map((post: PostType, idx: number) => (
+            <MainPostItem key={idx} post={post} />
+          ))}
+        </div>
+        {loading && <h1>로딩중이야요.</h1>}
+      </div>
+    </>
+  );
+};
+
+export default MainPosts;
