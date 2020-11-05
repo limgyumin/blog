5ff7ff5cf20a5b0e509462fa4e97622698c9f9d4
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-11-05 18:50:15 +0900

    [ADD] Login

---
+++ frontend/src/components/Login/Login.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Login.scss";
+
+interface LoginProps {
+
+}
+
+const Login = ({}: LoginProps) => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default Login;
