b093a1999c7db61d7980245344cdc125b4746e0f
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-11-05 10:30:03 +0900

    [UPDATE] Main Post Item UI

--- frontend/src/components/Main/MainPosts/MainPostItem/MainPostItem.tsx
+++ frontend/src/components/Main/MainPosts/MainPostItem/MainPostItem.tsx
@@ -3,19 +3,10 @@ import "./MainPostItem.scss";
 import moment from "moment";
 import chat from "../../../../assets/images/chat.svg";
 import like from "../../../../assets/images/like.svg";
+import PostType from "../../../../util/types/PostType";
 
 interface MainPostItemProps {
   post: PostType;
-}
-
-interface PostType {
-  idx: number;
-  title: string;
-  description: string;
-  thumbnail: string;
-  created_at: Date;
-  comment_count: number;
-  like_count: number;
 }
 
 const MainPostItem = ({ post }: MainPostItemProps) => {
@@ -24,15 +15,27 @@ const MainPostItem = ({ post }: MainPostItemProps) => {
       <div className="Main-Post-Item">
         <div className="Main-Post-Item-Thumbnail"></div>
         <div className="Main-Post-Item-Content">
+          <span className="Main-Post-Item-Content-Category">
+            <span>{post.category_name}</span> ·{" "}
+            {moment(post.created_at).format("YYYY년 MM월 DD일")}
+          </span>
           <span className="Main-Post-Item-Content-Title">{post.title}</span>
           <span className="Main-Post-Item-Content-Description">
             {post.description}
           </span>
         </div>
         <div className="Main-Post-Item-Bottom">
-          <span className="Main-Post-Item-Bottom-CreatedAt">
-            {moment(post.created_at).format("YYYY년 MM월 DD일")}
-          </span>
+          <div className="Main-Post-Item-Bottom-Profile">
+            <img
+              className="Main-Post-Item-Bottom-Profile-Avatar"
+              src={post.user_avatar}
+              alt={post.user_avatar}
+            />
+            <span className="Main-Post-Item-Bottom-Profile-Name">
+              <span>by </span>
+              {post.user_name}
+            </span>
+          </div>
           <div className="Main-Post-Item-Bottom-Count">
             <div className="Main-Post-Item-Bottom-Count-Comment">
               <img src={chat} alt={chat} />
