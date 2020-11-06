4527eb853043d54173077af08eb9aa03f40a188c
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-11-06 15:38:05 +0900

    [ADD] Login API

---
+++ frontend/src/assets/api/Login.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { SERVER } from "../../config/config.json";
+
+class Login {
+  async GitHubLogin(code: string) {
+    try {
+      const url = `${SERVER}/v1/auth/login`;
+      const body = {
+        code,
+      };
+
+      const { data } = await axios.post(url, body);
+      return data;
+    } catch (error) {
+      throw new Error(`${error}`);
+    }
+  }
+}
+
+export default new Login();
