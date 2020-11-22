528eea43d7ee9a7b54dc777ebb2974d16fd2bb82
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-11-22 19:23:43 +0900

    [UPDATE] 로그인 완료 시 User Avatar을 헤더에 표시

--- frontend/src/components/common/Header/Header.tsx
+++ frontend/src/components/common/Header/Header.tsx
@@ -4,13 +4,17 @@ import { ReactComponent as Logo } from "../../../assets/images/logo.svg";
 import { ReactComponent as GitHub } from "../../../assets/images/github_logo.svg";
 import { OAUTH } from "../../../config/config.json";
 import { Link } from "react-router-dom";
+import UserType from "../../../util/types/User";
 
 interface HeaderProps {
   shadow: boolean;
   hide: boolean;
+  admin: boolean;
+  login: boolean;
+  user?: UserType;
 }
 
-const Header = ({ shadow, hide }: HeaderProps) => {
+const Header = ({ shadow, hide, admin, login, user }: HeaderProps) => {
   return (
     <>
       <div
@@ -27,9 +31,19 @@ const Header = ({ shadow, hide }: HeaderProps) => {
             <Logo className="Header-Container-Image" />
           </Link>
           <div className="Header-Container-Profile">
-            <a href={OAUTH} className="Header-Container-Profile-Button">
-              <GitHub className="Header-Container-Profile-Button-Logo" />
-            </a>
+            {login ? (
+              <>
+                <img
+                  src={user?.avatar}
+                  alt="Profile"
+                  className="Header-Container-Profile-Avatar"
+                />
+              </>
+            ) : (
+              <a href={OAUTH} className="Header-Container-Profile-Button">
+                <GitHub className="Header-Container-Profile-Button-Logo" />
+              </a>
+            )}
           </div>
         </div>
       </div>
