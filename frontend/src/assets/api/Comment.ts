42396529cb9e73e5cf2fbc1c6d7f3f2c63d48851
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-12-02 16:21:57 +0900

    [ADD] 댓글 목록 조회, 댓글 쓰기, 댓글 개수 조회 API

---
+++ frontend/src/assets/api/Comment.ts
@@ -0,0 +1,47 @@
+import { SERVER } from "../../config/config.json";
+import axios from "axios";
+
+class Comment {
+  async GetCommentCount(idx: number) {
+    try {
+      const url = `${SERVER}/v1/post/comment/${idx}`;
+
+      const { data } = await axios.get(url);
+
+      return data;
+    } catch (error) {
+      throw new Error(`${error}`);
+    }
+  }
+
+  async CreateComment(post_idx: number, content: string) {
+    try {
+      const url = `${SERVER}/v1/comment`;
+
+      const body = {
+        post_idx,
+        content,
+      };
+
+      const { data } = await axios.post(url, body);
+
+      return data;
+    } catch (error) {
+      throw new Error(`${error}`);
+    }
+  }
+
+  async GetComments(post: number) {
+    try {
+      const url = `${SERVER}/v1/comment?post=${post}`;
+
+      const { data } = await axios.get(url);
+
+      return data;
+    } catch (error) {
+      throw new Error(`${error}`);
+    }
+  }
+}
+
+export default new Comment();
