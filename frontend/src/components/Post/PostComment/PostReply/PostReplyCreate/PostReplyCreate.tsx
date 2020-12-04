3bb81c98bc81e33457a57a157599103781d9508d
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-12-04 15:07:14 +0900

    [ADD] Post Reply Create

---
+++ frontend/src/components/Post/PostComment/PostReply/PostReplyCreate/PostReplyCreate.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./PostReplyCreate.scss";
+
+interface PostReplyCreateProps {
+  content: string;
+  setContent: React.Dispatch<React.SetStateAction<string>>;
+  handleCreateReplyCallback: () => Promise<void>;
+  handleCreateCancel: () => void;
+  keyDownListener: (e: React.KeyboardEvent<HTMLTextAreaElement>) => void;
+}
+
+const PostReplyCreate = ({
+  content,
+  setContent,
+  handleCreateReplyCallback,
+  handleCreateCancel,
+  keyDownListener,
+}: PostReplyCreateProps) => {
+  return (
+    <>
+      <div className="Post-Reply-Create">
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          onKeyDown={(e) => keyDownListener(e)}
+          placeholder="답글을 작성해주세요."
+          className="Post-Reply-Create-Box"
+        />
+        <div className="Post-Reply-Create-Wrapper">
+          <button
+            className="Post-Reply-Create-Wrapper-Button"
+            onClick={() => handleCreateReplyCallback()}
+          >
+            작성하기
+          </button>
+          <button
+            className="Post-Reply-Create-Wrapper-Cancel"
+            onClick={() => handleCreateCancel()}
+          >
+            취소
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PostReplyCreate;
