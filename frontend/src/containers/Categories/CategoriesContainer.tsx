e449891c8a9db805c4e7eb1d34c90163f557ffb7
limgyumin <52942566+limgyumin@users.noreply.github.com>
2020-12-25 16:42:06 +0900

    [ADD] Categories Container & Page

---
+++ frontend/src/containers/Categories/CategoriesContainer.tsx
@@ -0,0 +1,31 @@
+import React, { useCallback, useEffect } from "react";
+import { observer } from "mobx-react";
+import Categories from "../../components/Categories";
+import useStore from "../../util/lib/hooks/useStore";
+import { CategoryPostsResponse } from "../../util/types/Response";
+import { toast } from "react-toastify";
+
+const CategoriesContainer = ({}) => {
+  const { store } = useStore();
+  const { categoryPosts, handleCategoryPosts } = store.CategoryStore;
+
+  const handleCategoryPostsCallback = useCallback(async () => {
+    await handleCategoryPosts()
+      .then((res: CategoryPostsResponse) => {})
+      .catch((err: Error) => {
+        toast.error("이런! 카테고리 조회에 실패했어요.");
+      });
+  }, []);
+
+  useEffect(() => {
+    handleCategoryPostsCallback();
+  }, [handleCategoryPostsCallback]);
+
+  return (
+    <>
+      <Categories categoryPosts={categoryPosts} />
+    </>
+  );
+};
+
+export default observer(CategoriesContainer);
---
+++ frontend/src/pages/CategoriesPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Template from "../components/common/Template/Template";
+import CategoriesContainer from "../containers/Categories/CategoriesContainer";
+
+const CategoriesPage = () => {
+  return (
+    <>
+      <CategoriesContainer />
+    </>
+  );
+};
+
+export default CategoriesPage;
