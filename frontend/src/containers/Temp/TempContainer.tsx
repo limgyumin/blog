0769f5565eac48ccf708063dc420d8d83dd2ec31
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-01-02 23:17:31 +0900

    [FIX] 임시 글 조회 페이지는 관리자 만 접근 할 수 있도록 수정

--- frontend/src/containers/Temp/TempContainer.tsx
+++ frontend/src/containers/Temp/TempContainer.tsx
@@ -2,24 +2,69 @@ import React, { useCallback, useEffect, useState } from "react";
 import { observer } from "mobx-react";
 import Temp from "../../components/Temp";
 import useStore from "../../util/lib/hooks/useStore";
-import { PostsResponse } from "../../util/types/Response";
+import { PostsResponse, ProfileResponse } from "../../util/types/Response";
 import PostType from "../../util/types/Post";
 import { toast } from "react-toastify";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 const TempContainer = ({}) => {
+  const history = useHistory();
   const { store } = useStore();
+  const {
+    admin,
+    login,
+    handleLoginState,
+    handleAdminState,
+    handleMyProfile,
+  } = store.UserStore;
   const { handleTempPosts } = store.PostStore;
   const [tempPosts, setTempPosts] = useState<PostType[]>([]);
 
   const handleTempPostsCallback = useCallback(async () => {
-    await handleTempPosts()
-      .then((res: PostsResponse) => {
-        setTempPosts(res.data.posts);
-      })
-      .catch((err) => {
-        toast.error("으악! 임시 글 목록 조회에 실패했어요.");
-      });
+    if (admin && login) {
+      await handleTempPosts()
+        .then((res: PostsResponse) => {
+          setTempPosts(res.data.posts);
+        })
+        .catch((err) => {
+          toast.error("으악! 임시 글 목록 조회에 실패했어요.");
+        });
+    } else {
+      history.push("/");
+    }
+  }, [admin, login]);
+
+  const handleAdminCallback = useCallback(async () => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      handleLoginState(true);
+      axios.defaults.headers.common["access_token"] = token;
+
+      await handleMyProfile()
+        .then((res: ProfileResponse) => {
+          if (res.data.user.is_admin) {
+            handleAdminState(true);
+          } else {
+            history.push("/");
+          }
+        })
+        .catch((err: Error) => {
+          if (err.message.indexOf("410")) {
+            localStorage.removeItem("access_token");
+            handleLoginState(false);
+            axios.defaults.headers.common["access_token"] = "";
+          }
+        });
+    } else {
+      handleLoginState(false);
+      history.push("/");
+    }
   }, []);
+
+  useEffect(() => {
+    handleAdminCallback();
+  }, [handleAdminCallback]);
 
   useEffect(() => {
     handleTempPostsCallback();
