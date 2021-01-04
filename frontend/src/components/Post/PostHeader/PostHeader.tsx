670ae106f36a7c1031b141ed3dfc6d3532e57d4c
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-01-04 23:29:01 +0900

    [DELETE] Post Like

--- frontend/src/components/Post/PostHeader/PostHeader.tsx
+++ frontend/src/components/Post/PostHeader/PostHeader.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { ReactComponent as Like } from "../../../assets/images/like.svg";
 import { FaTrash, FaPen } from "react-icons/fa";
 import getTimeCount from "../../../util/lib/getTimeCount";
-import PostLike from "../PostLike";
 import "./PostHeader.scss";
 
 interface PostHeaderProps {
@@ -11,11 +9,8 @@ interface PostHeaderProps {
   categoryName: string;
   createdAt: Date;
   thumbnail: string;
-  handlePostLikeCallback: () => Promise<void>;
   showModalCallback: () => void;
   modifyClickHandler: () => void;
-  likeCount: number;
-  liked: boolean;
   admin: boolean;
 }
 
@@ -25,11 +20,8 @@ const PostHeader = ({
   categoryName,
   createdAt,
   thumbnail,
-  handlePostLikeCallback,
   showModalCallback,
   modifyClickHandler,
-  likeCount,
-  liked,
   admin,
 }: PostHeaderProps) => {
   return (
@@ -51,31 +43,6 @@ const PostHeader = ({
               </p>
             </div>
             <div className="Post-Header-Container-Category-Right">
-              <div
-                className={
-                  liked
-                    ? "Post-Header-Container-Category-Right-Like-Active Post-Header-Container-Category-Right-Like"
-                    : "Post-Header-Container-Category-Right-Like"
-                }
-                onClick={handlePostLikeCallback}
-              >
-                <Like
-                  className={
-                    liked
-                      ? "Post-Header-Container-Category-Right-Like-Button-Active Post-Header-Container-Category-Right-Like-Button"
-                      : "Post-Header-Container-Category-Right-Like-Button"
-                  }
-                />
-                <span
-                  className={
-                    liked
-                      ? "Post-Header-Container-Category-Right-Like-Count-Active Post-Header-Container-Category-Right-Like-Count"
-                      : "Post-Header-Container-Category-Right-Like-Count"
-                  }
-                >
-                  {likeCount}
-                </span>
-              </div>
               {admin && (
                 <div className="Post-Header-Container-Category-Right-Control">
                   <FaPen onClick={() => modifyClickHandler()} />
@@ -84,13 +51,6 @@ const PostHeader = ({
               )}
             </div>
           </div>
-        </div>
-        <div className="Post-Header-Like">
-          <PostLike
-            handlePostLikeCallback={handlePostLikeCallback}
-            likeCount={likeCount}
-            liked={liked}
-          />
         </div>
         {thumbnail && (
           <img
