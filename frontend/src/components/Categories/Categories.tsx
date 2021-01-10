0afc8cdce5a02fd341e6e4782c83af3c054997bc
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-01-11 04:58:32 +0900

    [ADD] 카테고리 생성 기능

--- frontend/src/components/Categories/Categories.tsx
+++ frontend/src/components/Categories/Categories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaExchangeAlt, FaPen, FaTrash } from "react-icons/fa";
-import { RiFolderAddFill } from "react-icons/ri";
+import { IoMdAdd } from "react-icons/io";
 import { CategoryPostsType } from "../../util/types/Category";
 import "./Categories.scss";
 import CategoriesList from "./CategoriesListItem";
@@ -9,32 +9,76 @@ interface CategoriesProps {
   categoryPosts: CategoryPostsType[];
   admin: boolean;
   login: boolean;
+  categoryName: string;
+  createMode: boolean;
+  modifyMode: boolean;
   deleteMode: boolean;
+  setCreateMode: React.Dispatch<React.SetStateAction<boolean>>;
+  setModifyMode: React.Dispatch<React.SetStateAction<boolean>>;
   setDeleteMode: React.Dispatch<React.SetStateAction<boolean>>;
+  setCategoryName: React.Dispatch<React.SetStateAction<string>>;
   deleteCategoryHandler: (idx: number) => void;
+  keyDownListener: (e: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 
 const Categories = ({
   categoryPosts,
   admin,
   login,
+  categoryName,
+  createMode,
+  modifyMode,
   deleteMode,
+  setCreateMode,
+  setModifyMode,
   setDeleteMode,
+  setCategoryName,
   deleteCategoryHandler,
+  keyDownListener,
 }: CategoriesProps) => {
   return (
     <>
       <div className="Categories">
         <div className="Categories-Wrapper">
+          {admin && login && (
+            <div className="Categories-Wrapper-Container">
+              <div
+                className={
+                  createMode
+                    ? "Categories-Wrapper-Container-Name-Active Categories-Wrapper-Container-Name"
+                    : "Categories-Wrapper-Container-Name"
+                }
+              >
+                <input
+                  className="Categories-Wrapper-Container-Name-Input"
+                  type="text"
+                  placeholder="카테고리 이름을 입력해주세용."
+                  value={categoryName}
+                  onChange={(e) => setCategoryName(e.target.value)}
+                  onKeyDown={(e) => keyDownListener(e)}
+                />
+              </div>
+              <button
+                className="Categories-Wrapper-Container-Create"
+                onClick={() => setCreateMode((prev) => !prev)}
+              >
+                <IoMdAdd
+                  className={
+                    createMode
+                      ? "Categories-Wrapper-Container-Create-Icon-Active Categories-Wrapper-Container-Create-Icon"
+                      : "Categories-Wrapper-Container-Create-Icon"
+                  }
+                />
+                <p className="Categories-Wrapper-Container-Create-Content">
+                  {createMode ? "취소" : "카테고리 추가"}
+                </p>
+              </button>
+            </div>
+          )}
           <div className="Categories-Wrapper-Header">
             <p className="Categories-Wrapper-Header-Title">Categories</p>
             {admin && login && (
               <div className="Categories-Wrapper-Header-Control">
-                {!deleteMode && (
-                  <div className="Categories-Wrapper-Header-Control-Add">
-                    <RiFolderAddFill />
-                  </div>
-                )}
                 {!deleteMode && (
                   <div className="Categories-Wrapper-Header-Control-Edit">
                     <FaPen />
--- frontend/src/containers/Categories/CategoriesContainer.tsx
+++ frontend/src/containers/Categories/CategoriesContainer.tsx
@@ -2,26 +2,55 @@ import React, { useCallback, useEffect, useState } from "react";
 import { observer } from "mobx-react";
 import Categories from "../../components/Categories";
 import useStore from "../../util/lib/hooks/useStore";
-import { CategoryPostsResponse } from "../../util/types/Response";
+import {
+  CategoriesResponse,
+  CategoryPostsResponse,
+} from "../../util/types/Response";
 import { toast } from "react-toastify";
 import ModalContainer from "../Modal/ModalContainer";
 import Portal from "../../components/common/Portal";
 import CategoryDeleteAlert from "../../components/Categories/CategoryDeleteAlert";
+import isEmpty from "../../util/lib/isEmpty";
+import removeLastBlank from "../../util/lib/removeLastBlank";
 
 const CategoriesContainer = ({}) => {
   const { store } = useStore();
   const {
     categoryPosts,
+    handleCreateCategory,
+    handleCategories,
     handleCategoryPosts,
     handleDeleteCategory,
   } = store.CategoryStore;
   const { admin, login } = store.UserStore;
 
-  const [categoryIdx, setCategoryIdx] = useState<number>(0);
-  const [modifyMode, setModifyMode] = useState<boolean>(false);
-  const [deleteMode, setDeleteMode] = useState<boolean>(false);
   const [isShow, setIsShow] = useState<boolean>(false);
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const [categoryName, setCategoryName] = useState<string>("");
+  const [categoryIdx, setCategoryIdx] = useState<number>(0);
+
+  const [createMode, setCreateMode] = useState<boolean>(false);
+  const [modifyMode, setModifyMode] = useState<boolean>(false);
+  const [deleteMode, setDeleteMode] = useState<boolean>(false);
+
+  const handleCreateCategoryCallback = useCallback(async () => {
+    if (admin && login) {
+      await handleCreateCategory(removeLastBlank(categoryName))
+        .then((res: Response) => {
+          handleCategoryPostsCallback();
+          handleCategoriesCallback();
+          setCreateMode(false);
+        })
+        .catch((err: Error) => {
+          if (err.message.indexOf("409")) {
+            toast.error("중복된 카테고리에요.");
+          } else {
+            toast.error("악! 카테고리 생성에 실패했어요.");
+          }
+        });
+    }
+  }, [categoryName]);
 
   const handleCategoryPostsCallback = useCallback(async () => {
     await handleCategoryPosts()
@@ -31,14 +60,25 @@ const CategoriesContainer = ({}) => {
       });
   }, []);
 
-  const handleDeleteCategoryCallback = useCallback(async () => {
-    await handleDeleteCategory(categoryIdx)
-      .then(() => {
-        handleCategoryPostsCallback();
-      })
+  const handleCategoriesCallback = useCallback(async () => {
+    await handleCategories()
+      .then((res: CategoriesResponse) => {})
       .catch((err: Error) => {
-        toast.error("이런! 카테고리 삭제에 실패했어요.");
+        toast.error("이런! 카테고리 조회에 실패했어요.");
       });
+  }, []);
+
+  const handleDeleteCategoryCallback = useCallback(async () => {
+    if (admin && login) {
+      await handleDeleteCategory(categoryIdx)
+        .then(() => {
+          handleCategoryPostsCallback();
+          handleCategoriesCallback();
+        })
+        .catch((err: Error) => {
+          toast.error("이런! 카테고리 삭제에 실패했어요.");
+        });
+    }
   }, [categoryIdx]);
 
   const showModalCallback = useCallback(() => {
@@ -56,6 +96,18 @@ const CategoriesContainer = ({}) => {
     showModalCallback();
     setCategoryIdx(idx);
   };
+
+  const keyDownListener = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (
+        !isEmpty(categoryName) &&
+        (e.key === "Enter" || e.key === "NumpadEnter")
+      ) {
+        handleCreateCategoryCallback();
+      }
+    },
+    [categoryName, handleCreateCategoryCallback]
+  );
 
   const deleteClickHandler = useCallback(() => {
     handleDeleteCategoryCallback();
@@ -80,9 +132,16 @@ const CategoriesContainer = ({}) => {
         categoryPosts={categoryPosts}
         admin={admin}
         login={login}
+        categoryName={categoryName}
+        createMode={createMode}
+        modifyMode={modifyMode}
         deleteMode={deleteMode}
+        setCreateMode={setCreateMode}
+        setModifyMode={setModifyMode}
         setDeleteMode={setDeleteMode}
+        setCategoryName={setCategoryName}
         deleteCategoryHandler={deleteCategoryHandler}
+        keyDownListener={keyDownListener}
       />
     </>
   );
