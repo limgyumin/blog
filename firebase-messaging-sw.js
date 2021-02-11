361d497961f702bca80c1ccefba5ca123b1e57a5
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-02-11 20:13:02 +0900

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,26 +1,30 @@
-importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
+if ("function" === typeof importScripts) {
+  importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
+  importScripts(
+    "https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js"
+  );
 
-firebase.initializeApp({
-  messagingSenderId: "969362574757",
-});
+  firebase.initializeApp({
+    messagingSenderId: "969362574757",
+  });
 
-const messaging = firebase.messaging();
-messaging.setBackgroundMessageHandler(function (payload) {
-  const promiseChain = clients
-    .matchAll({
-      type: "window",
-      includeUncontrolled: true,
-    })
-    .then((windowClients) => {
-      for (let i = 0; i < windowClients.length; i++) {
-        const windowClient = windowClients[i];
-        windowClient.postMessage(payload);
-      }
-    })
-    .then(() => {
-      return registration.showNotification("my notification title");
-    });
-  return promiseChain;
-});
-self.addEventListener("notificationclick", function (event) {});
+  const messaging = firebase.messaging();
+  messaging.setBackgroundMessageHandler(function (payload) {
+    const promiseChain = clients
+      .matchAll({
+        type: "window",
+        includeUncontrolled: true,
+      })
+      .then((windowClients) => {
+        for (let i = 0; i < windowClients.length; i++) {
+          const windowClient = windowClients[i];
+          windowClient.postMessage(payload);
+        }
+      })
+      .then(() => {
+        return registration.showNotification("my notification title");
+      });
+    return promiseChain;
+  });
+  self.addEventListener("notificationclick", function (event) {});
+}
