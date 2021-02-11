ed00c2b54f65da519f60a5db9c6cb0f6142a8486
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-02-11 20:38:06 +0900

    Updates

---
+++ precache-manifest.e56ce5ad58498e7552c79ef15de07b5c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8ccefea2109ae15442dd4cdceca225",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98f517bda89bb0fc6c32",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "6a80cb6936221e6eb9a9",
+    "url": "/static/css/main.6554328e.chunk.css"
+  },
+  {
+    "revision": "98f517bda89bb0fc6c32",
+    "url": "/static/js/2.2e86098b.chunk.js"
+  },
+  {
+    "revision": "1a1d1926f0c79f35c99dd1a702488a20",
+    "url": "/static/js/2.2e86098b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a80cb6936221e6eb9a9",
+    "url": "/static/js/main.140743d4.chunk.js"
+  },
+  {
+    "revision": "880ead2686ec4d3085c1",
+    "url": "/static/js/runtime-main.b40e28e2.js"
+  },
+  {
+    "revision": "089d59a5d037d968e53c57ab9fa5d507",
+    "url": "/static/media/MalgunGothic-Bold.089d59a5.woff2"
+  },
+  {
+    "revision": "2ec7e13c035c9e6288e4f82badb80f38",
+    "url": "/static/media/MalgunGothic-Bold.2ec7e13c.woff"
+  },
+  {
+    "revision": "a56e19c0f8ff735e0444cc7cee107b9a",
+    "url": "/static/media/MalgunGothic-Bold.a56e19c0.eot"
+  },
+  {
+    "revision": "f9b735d738c24773631773684b05298d",
+    "url": "/static/media/MalgunGothic-Bold.f9b735d7.otf"
+  },
+  {
+    "revision": "021f989aa5bb9aee2a336a13bffd6017",
+    "url": "/static/media/MalgunGothic-Regular.021f989a.woff2"
+  },
+  {
+    "revision": "1d0b24ee09df1f3f3f885e007e8149fe",
+    "url": "/static/media/MalgunGothic-Regular.1d0b24ee.woff"
+  },
+  {
+    "revision": "69b2dab34beab4d2590b7b608c4343e7",
+    "url": "/static/media/MalgunGothic-Regular.69b2dab3.otf"
+  },
+  {
+    "revision": "7cbdca0f6f9335293bd472a603e50bdf",
+    "url": "/static/media/MalgunGothic-Regular.7cbdca0f.eot"
+  },
+  {
+    "revision": "06fe47412711c500a286eaabd220cffe",
+    "url": "/static/media/MalgunGothic-Semilight.06fe4741.otf"
+  },
+  {
+    "revision": "2f3fa0b832f315fcd478bd0edab2d856",
+    "url": "/static/media/MalgunGothic-Semilight.2f3fa0b8.eot"
+  },
+  {
+    "revision": "5de512596a02c6661975ecde25218de2",
+    "url": "/static/media/MalgunGothic-Semilight.5de51259.woff2"
+  },
+  {
+    "revision": "9aebcd8011c7cf5d2eddecaa681957bf",
+    "url": "/static/media/MalgunGothic-Semilight.9aebcd80.woff"
+  },
+  {
+    "revision": "36e0b5f06a5e0781236ea1b3e2727c0e",
+    "url": "/static/media/github.36e0b5f0.svg"
+  },
+  {
+    "revision": "c6404cae8e1074da267037100e4083ba",
+    "url": "/static/media/github_logo.c6404cae.svg"
+  },
+  {
+    "revision": "3e9b74103e96286da49e0ca36556c8ee",
+    "url": "/static/media/logo.3e9b7410.svg"
+  },
+  {
+    "revision": "305b54d5e327d5299c2d7d372a86a869",
+    "url": "/static/media/not_found.305b54d5.svg"
+  },
+  {
+    "revision": "147571c1082b209a75dce54d205763be",
+    "url": "/static/media/option.147571c1.svg"
+  },
+  {
+    "revision": "96395aef1bf44b0c8b9d2271dacdd4d0",
+    "url": "/static/media/paper.96395aef.svg"
+  },
+  {
+    "revision": "c23d774451732175b78189deb5114949",
+    "url": "/static/media/shadow.c23d7744.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5fb49343e41c69d19a613362e97d2f5f.js"
+  "/precache-manifest.e56ce5ad58498e7552c79ef15de07b5c.js"
 );
 
 self.addEventListener('message', (event) => {
