4d40ff2dda70576d4644aa7a733434f73a13eac0
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-06-22 14:41:09 +0900

    ADD: Custom Hook으로 사용자 목록 컴포넌트 로직 분리

--- frontend/src/components/Members/MembersUserItem/MembersUserItem.tsx
+++ frontend/src/components/Members/MembersUserItem/MembersUserItem.tsx
@@ -1,42 +1,39 @@
-import React from "react";
-import getDateFormat from "../../../util/lib/getDateFormat";
-import UserType from "../../../util/types/User";
-import "./MembersUserItem.scss";
+import classNames from "classnames";
+import { ClassNamesFn } from "classnames/types";
+import React, { FC } from "react";
+import IUser from "types/user.type";
+import getDateFormat from "../../../lib/getDateFormat";
 
-interface MembersUserItemProps {
-  user: UserType | Partial<UserType>;
-}
+const styles = require("./MembersUserItem.scss");
+const cx: ClassNamesFn = classNames.bind(styles);
 
-const MembersUserItem = ({ user }: MembersUserItemProps) => {
+type MembersUserItemProps = {
+  user: IUser;
+};
+
+const MembersUserItem: FC<MembersUserItemProps> = ({ user }) => {
   return (
-    <>
-      <div className="Members-User-Item">
-        <a
-          href={`https://github.com/${user.id}`}
-          target="_blank"
-          className="Members-User-Item-Container"
-        >
-          <div className="Members-User-Item-Container-Wrapper">
-            <img
-              className="Members-User-Item-Container-Wrapper-Avatar"
-              src={user.avatar}
-              alt={user.avatar}
-            />
-            <div className="Members-User-Item-Container-Wrapper-Info">
-              <p className="Members-User-Item-Container-Wrapper-Info-Name">
-                {user.name}
-              </p>
-              <p className="Members-User-Item-Container-Wrapper-Info-Bio">
-                {user.bio}
-              </p>
-            </div>
+    <div className={cx("members-user-item")}>
+      <a
+        className={cx("members-user-item-wrap")}
+        href={`https://github.com/${user.id}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className={cx("members-user-item-wrap-container")}>
+          <img
+            className={cx("members-user-item-wrap-container-avatar")}
+            src={user.avatar}
+            alt={user.avatar}
+          />
+          <div className={cx("members-user-item-wrap-container-info")}>
+            <p className={cx("members-user-item-wrap-container-info-name")}>{user.name}</p>
+            <p className={cx("members-user-item-wrap-container-info-bio")}>{user.bio}</p>
           </div>
-        </a>
-        <p className="Members-User-Item-CreatedAt">
-          {getDateFormat(user.created_at!)}
-        </p>
-      </div>
-    </>
+        </div>
+      </a>
+      <p className={cx("members-user-item-date")}>{getDateFormat(user.created_at!)}</p>
+    </div>
   );
 };
 
