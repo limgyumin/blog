145dc6f18a8c3ec2e0399db45dcd3f748cd54579
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-06-29 09:16:03 +0900

    FIX: 임시 글 생성 시 생성된 글의 idx를 주도록 수정

--- backend/src/lib/validation/post.ts
+++ backend/src/lib/validation/post.ts
@@ -16,9 +16,9 @@ export const validateCreate = (req: Request, res: Response): boolean => {
 
 export const validateModify = (req: Request, res: Response): boolean => {
   const schema = Joi.object().keys({
-    title: Joi.string().min(1).max(255),
+    title: Joi.string().max(255).required(),
     description: Joi.string().max(255),
-    content: Joi.string(),
+    content: Joi.string().required(),
     thumbnail: Joi.string().max(800).allow(null).allow(""),
     category_idx: Joi.number().integer().allow(null),
     is_temp: Joi.boolean(),
@@ -30,10 +30,10 @@ export const validateModify = (req: Request, res: Response): boolean => {
 export const validateCreateTemp = (req: Request, res: Response): boolean => {
   const schema = Joi.object().keys({
     title: Joi.string().max(255).required(),
-    content: Joi.string().min(1).max(255).required(),
-    thumbnail: Joi.string().max(800).allow(null).allow(""),
-    category_idx: Joi.number().integer().allow(null).allow(""),
     description: Joi.string().max(255).allow(null).allow(""),
+    content: Joi.string().required(),
+    thumbnail: Joi.string().max(800).allow(null).allow(""),
+    category_idx: Joi.number().integer().allow(null),
   });
 
   return validation(req, res, schema);
