304fc6b014133fc023f70080b87d71742a0d622c
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-07-01 11:07:11 +0900

    FIX: asset 이미지 대신 react icons 사용

--- frontend/src/components/common/Header/HeaderAction/HeaderAction.tsx
+++ frontend/src/components/common/Header/HeaderAction/HeaderAction.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { OAUTH } from "config/config.json";
-import { ReactComponent as GitHub } from "assets/images/github_logo.svg";
+import { AiFillGithub } from "react-icons/ai";
 import classNames from "classnames";
 import { ClassNamesFn } from "classnames/types";
 
@@ -10,7 +10,7 @@ const cx: ClassNamesFn = classNames.bind(styles);
 const HeaderAction = () => {
   return (
     <a href={OAUTH} className={cx("header-action")}>
-      <GitHub className={cx("header-action-logo")} />
+      <AiFillGithub className={cx("header-action-icon")} />
     </a>
   );
 };
