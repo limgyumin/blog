b2f4a3d050dde81d7c5791cf6e8185d3432ff80f
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-07-01 11:06:16 +0900

    ADD: Header Menu Tab

---
+++ frontend/src/components/common/Header/HeaderMenuTab/HeaderMenuTab.tsx
@@ -0,0 +1,21 @@
+import classNames from "classnames";
+import { ClassNamesFn } from "classnames/types";
+import { headerMenuTabModel } from "components/models/headerMenuTabModel";
+import React from "react";
+import { memo } from "react";
+import MenuTabItem from "./MenuTabItem";
+
+const styles = require("./HeaderMenuTab.scss");
+const cx: ClassNamesFn = classNames.bind(styles);
+
+const HeaderMenuTab = () => {
+  return (
+    <div className={cx("header-menu-tab")}>
+      {headerMenuTabModel.map((tabMenu, idx) => (
+        <MenuTabItem key={idx} tabMenu={tabMenu} />
+      ))}
+    </div>
+  );
+};
+
+export default memo(HeaderMenuTab);
