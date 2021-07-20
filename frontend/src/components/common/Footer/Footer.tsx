480a7a2ad4c0f9eba9cd284a06759c2bc4052539
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-07-20 15:45:30 +0900

    FIX: Footer styled-components 적용

--- frontend/src/components/common/Footer/Footer.tsx
+++ frontend/src/components/common/Footer/Footer.tsx
@@ -1,18 +1,36 @@
-import classNames from "classnames";
-import { ClassNamesFn } from "classnames/types";
 import React from "react";
-
-const styles = require("./Footer.scss");
-const cx: ClassNamesFn = classNames.bind(styles);
+import styled from "styled-components";
 
 const Footer = () => {
   return (
-    <footer className={cx("footer")}>
-      <div className={cx("footer-wrap")}>
-        <p className={cx("footer-wrap-content")}>© 2021. limgyumin. All rights reserved.</p>
-      </div>
-    </footer>
+    <FooterWrapper>
+      <FooterContainer>
+        <FooterContent>© 2021. limgyumin. All rights reserved.</FooterContent>
+      </FooterContainer>
+    </FooterWrapper>
   );
 };
+
+const FooterWrapper = styled.footer`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FooterContainer = styled.div`
+  border-top: 1px solid ${({ theme }) => theme.color.bdColor};
+  width: 100%;
+  max-width: 1218px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1.5rem 2rem 2.5rem 2rem;
+`;
+
+const FooterContent = styled.p`
+  font-size: 0.925rem;
+  color: ${({ theme }) => theme.color.ftColor5};
+`;
 
 export default Footer;
